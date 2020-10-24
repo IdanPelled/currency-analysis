4090dc5809623a282423cdb1b8e816d3ac478f18
Idan Pelled <pelled.idan@gmail.com>
2020-10-24 12:41:10 +0300

    added disclaimer and removed 404 error page

--- app.py
+++ app.py
@@ -11,38 +11,40 @@ def home_page():
     return render_template("index.html")
 
 
-@app.route('/search')
+@app.route('/search', methods=['POST', "GET"])
 def search_page():
     """Search result page."""
-    args = request.args
-    base = args.get("base-currency")
-    currencies = args.getlist("secondary-currency")
-    time = args.get("time-frame")
+    if request.method == 'POST':
+        form = request.form
+        base = form.get("base-currency")
+        currencies = form.getlist("secondary-currency")
+        time = form.get("time-frame")
 
-    plot = visuals(base, currencies, time)
-    if isinstance(plot, str):
-        return render_template(
-            "search.html",
-            base=base,
-            currencies=currencies,
-            time=time,
-            plot=plot,
-        )
-    elif plot is None:
-        # if the plot parms are un-valid plot
-        print("input error")
-        return render_template('index.html')
-    elif not plot:
-        # if there was an error message from the API
-        # or the page was not found
-        print("API error")
-    return render_template('404.html')
+        plot = visuals(base, currencies, time)
+        if isinstance(plot, str):
+            return render_template(
+                "search.html",
+                base=base,
+                currencies=currencies,
+                time=time,
+                plot=plot,
+            )
+        elif plot is None:
+            # if the plot parms are un-valid plot
+            return render_template('index.html')
+        elif not plot:
+            # if there was an error message from the API
+            # or the page was not found
+            return "<h1>API Error</h1><p>We are having some problems, try again soon."
+    else:
+        # if the user tries to go to the search page with a GET request.
+        return '<h1>Error</h1><p>Please fill the form to get to the "search page".'
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """404 page_not_found error page."""
-    return render_template('404.html'), 404
+@app.route('/disclaimer')
+def disclaimer():
+    """Disclaimer page."""
+    return render_template("disclaimer.html")
 
 
 if __name__ == "__main__":
